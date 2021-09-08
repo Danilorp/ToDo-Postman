c70b3842e45abcb69a2f498e82fee8da76565b8a
Danilo Pinha <danilorpinha@hotmail.com>
2021-09-08 19:27:51 -0300

    Add project files.

---
+++ ToDo-Postman/Context/TodoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDo_Postman.Model;
+
+namespace ToDo_Postman.Context
+{
+    public class TodoContext : DbContext
+    {
+
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ ToDo-Postman/Model/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDo_Postman.Model
+{
+    public class TodoItem
+    {
+        public long id { get; set; }
+
+        public string Nome { get; set; }
+
+        public bool EstaCompleto { get; set; }
+    }
+}
